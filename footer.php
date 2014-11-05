bdf9a29a1cc80106df161788d68da47b2b7571ce
AirScript <airscript@outlook.com>
2014-11-05 21:54:20 +0800

    Upload Theme

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+        </div><!-- end .row -->
+    </div>
+</div><!-- end #body -->
+
+<footer id="footer" role="contentinfo">
+
+	Copyright &copy; 2013 - <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.All Rights Reserved.		<br>	    Powered by Typecho, Stored by CenMeo (China) Corporation		<br><a href="<?php $this->options->siteUrl(); ?>privacy-and-terms.html" target="_blank">Privacy&Terms</a>&nbsp;ICP License：<a href="http://www.miibeian.gov.cn/" target="_blank">蜀ICP备14009770号-3</a> <br>
+
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_5913726'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/stat.php%3Fid%3D5913726' type='text/javascript'%3E%3C/script%3E"));</script>
+</footer><!-- end #footer -->
+
+<?php $this->footer(); ?>
+</body>
+</html>
