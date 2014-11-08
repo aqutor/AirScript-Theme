549dfaae738064b3a4f24ead465a81e85e35da1b
AirScript <airscript@outlook.com>
2014-11-08 18:25:05 +0800

    Optimize SSL Experience
    1.Use local content to replace the content linked to airscr.com.
    2.Add copyright information.
    3.More things are waiting for you to discover.

--- footer.php
+++ footer.php
@@ -6,9 +6,8 @@
 
 <footer id="footer" role="contentinfo">
 
-	Copyright &copy; 2013 - <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.All Rights Reserved.		<br>	    Powered by Typecho, Stored by CenMeo (China) Corporation		<br><a href="<?php $this->options->siteUrl(); ?>privacy-and-terms.html" target="_blank">Privacy&Terms</a>&nbsp;ICP License：<a href="http://www.miibeian.gov.cn/" target="_blank">蜀ICP备14009770号-3</a> <br>
+	Copyright &copy; 2013 - <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.All Rights Reserved.		<br>	    Powered by <a href="http://typecho.org/" target="_blank">Typecho</a>, Theme by <a href="https://www.airscr.com/archives/823.html" target="_blank">AirScript</a>		<br> <a href="<?php $this->options->siteUrl(); ?>privacy-and-terms.html" target="_blank">Privacy&Terms</a> &nbsp;ICP License：<a href="http://www.miibeian.gov.cn/" target="_blank">蜀ICP备14009770号-3</a> <br>
 
-</footer><!-- end #footer -->
 
 <?php $this->footer(); ?>
 </body>
